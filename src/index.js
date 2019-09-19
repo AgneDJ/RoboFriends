6c1f972aee0fdc39964a3f13f3b181d3979b571e
Agne <fizikee@gmail.com>
2019-09-18 22:53:52 -0700

    WIP

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import 'tachyons';
+import { robots } from './robots.js';
+
+
+import * as serviceWorker from './serviceWorker';
+import App from './containers/App';
+
+ReactDOM.render(<App />	, document.getElementById('root'));
+serviceWorker.unregister();
---
+++ src/robots.js
@@ -0,0 +1,39 @@
+export const robots = [
+	{
+		id: 1,
+		name: 'Dalia Grybauskaite',
+		username: 'dalka',
+		email: 'dalite@gmail.com'
+	},
+	{
+		id: 2,
+		name: 'Olia Parkovska',
+		username: 'olka',
+		email: 'olite@gmail.com'
+	},
+	{
+		id: 3,
+		name: 'Ellen High',
+		username: 'ely',
+		email: 'ellen@gmail.com'
+	},
+	{
+		id: 4,
+		name: 'Armins Svogers',
+		username: 'armux',
+		email: 'armux@gmail.com'
+	},
+	{
+		id: 5,
+		name: 'Horst Witcherberg',
+		username: 'witch',
+		email: 'h.wi@gmail.com'
+	},
+	{
+		id: 6,
+		name: 'Ali Laban',
+		username: 'al.ban',
+		email: 'ali.b@gmail.com'
+	},
+	
+	]
